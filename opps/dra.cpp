8a2a0a5138255e54ae6a5c6313ae78ebe5974918
Kamal Barman <kamalbarman1528@gmail.com>
2024-03-28 07:27:14 -0400

    init

--- opps/dra.cpp
+++ opps/dra.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include<string>
+
 
 using namespace std;
 
@@ -7,26 +7,36 @@ class Employee{
 	private:
 		int empno;
 		char ename[20];
-		float basic;
-		float hra;
-		float da;
+		float basic,hra,da;
 	public:
 		void  getdata(){
 			cout << "enter employee number: ";
 			cin >> empno;
+
 			cout<< "enter employee name: ";
 			cin >> ename;
-			cout << "enter basic , hra , da";
-			cin >> basic >> hra >> da;
+
+			cout << "enter basic pay ";
+			cin>>basic;
+
+			cout << "enter hra ";
+			cin >>  hra;
+
+			cout << "enter da ";
+			cin >> da;
 					
 			
 		}
-	void calcudata(float np){
- 			np = basic+hra+da;
-			cout << np;
+	float calcudata(){
+ 			return basic + hra + da;
 		}
 	void dispdata(){
-			cout << "employee and name: "<< ename << empno;
+			cout << "employee number" << empno << endl;
+			cout << "employee name "<< empno<<endl;
+			cout << "basic pay "<< basic<<endl;
+			cout << "hra "<< hra<<endl;
+			cout << "da "<<da<< endl;
+			cout << "net pay"<<calcudata()<<endl;
 		 
 	}
 };
@@ -35,7 +45,6 @@ class Employee{
 int main(){
 	Employee std;
 	std.getdata();
-	std.calcudata();
 	std.dispdata();
 return 0;	
 }
